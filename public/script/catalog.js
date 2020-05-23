12f870348d3abff710c3ad1055f61005c24816c3
Sergio Inzunza <contact@sinzunza.com>
2020-05-23 11:13:34 -0700

    Class 4

--- public/script/catalog.js
+++ public/script/catalog.js
@@ -1,28 +1,28 @@
 
 
-var items=[];
-var serverURL="http://localhost:8080/API/";
+var items = [];
+var serverURL = "http://localhost:8080/API/";
 
-function fetchCatalog(){
+function fetchCatalog() {
     // get the items from the server
     $.ajax({
-        url:serverURL+"items",
-        type:"GET",
-        success:function(res){
-            console.log("it works",res);
+        url: serverURL + "items/Samatha",
+        type: "GET",
+        success: function (res) {
+            console.log("it works", res);
             // this will travel the array that the server sends back
-            for(var i=0;i<res.length;i++){
+            for (var i = 0; i < res.length; i++) {
                 var item = res[i];
-                drawItem(item); // send each item to be draw on HTML
+                drawItem(item); // send each item to be draw on HTML            
             }
         },
-        error:function(details){
-            console.log("Error",details);
+        error: function (details) {
+            console.log("Error", details);
         }
     });
 }
 
-function drawItem(product){
+function drawItem(product) {
     // create the template string 
     var layout = ` <div id="${product.code}" class="item">
                             <img src="${product.image}">
@@ -34,47 +34,47 @@ function drawItem(product){
                             </div>
                         </div> `;
     // display the layout on the DOM append
-         $('#catalog').append(layout);
+    $('#catalog').append(layout);
 }
 
-function Search(){
+function Search() {
     var searchText = $('#txt-search').val();
     $("#catalog").html("");
-     //travel the array and display the items that contains searchText in the title,
+    //travel the array and display the items that contains searchText in the title,
     // OR the description contains the searchText
     // OR the code contains the searchText
     // OR the price
-        for(var i=0;i<items.length;i++){
-            var item = items[i];
-            if(item.title.toLowerCase().includes(searchText) || item.description.toLowerCase().includes(searchText)){
-                drawItem(item);
-            }
+    for (var i = 0; i < items.length; i++) {
+        var item = items[i];
+        if (item.title.toLowerCase().includes(searchText) || item.description.toLowerCase().includes(searchText)) {
+            drawItem(item);
         }
+    }
 }
 
-function init(){
+function init() {
     console.log("Catalog Page");
     fetchCatalog();
 
     // detecting events
     $('#btn-search').click(Search);
 
-    $('#txt-search').change(function(){
+    $('#txt-search').change(function () {
         var searchText = $('#txt-search').val();
-        for(var i=0;i<items.length;i++){
-            if(searchText==''){
+        for (var i = 0; i < items.length; i++) {
+            if (searchText == '') {
                 //$('#'+items[i].code).show();
                 drawItem(items[i]);
             }
         }
     });
 
-    $('#txt-search').keypress(function(e){
-        if(e.keyCode==13){
+    $('#txt-search').keypress(function (e) {
+        if (e.keyCode == 13) {
             Search();
         }
     });
 
 }
 
-window.onload=init;
+window.onload = init;
--- server.js
+++ server.js
@@ -27,22 +27,29 @@ app.engine('html', ejs.renderFile);
 app.set('view engine', ejs);
 
 // To Server static files (css, js, images, pdf, doc, .... )
-app.use(express.static( __dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
+
+// Db connection with mongoose
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://ThiIsAPassword:TheRealPassword@cluster0-shard-00-00-euadh.mongodb.net:27017,cluster0-shard-00-01-euadh.mongodb.net:27017,cluster0-shard-00-02-euadh.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin');
+var mongoDB = mongoose.connection;
+var itemDB; // constructor for item objects
+var orderDB; // constructor for order objects
 
 
 /********************************************************** */
 /** Web Server endpoints */
 /********************************************************** */
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.render('index.html');
 });
 
-app.get('/admin', function(req, res) {
+app.get('/admin', function (req, res) {
     res.render('admin.html');
 });
 
-app.get('/about', function(req, res){
+app.get('/about', function (req, res) {
     res.render('about.html');
 });
 
@@ -54,36 +61,116 @@ app.get('/contact', (req, res) => {
 /** REST API endpoints */
 /********************************************************** */
 
-var db = [];
-var lastId = 1;
 
 app.post('/api/items', (req, res) => {
-    var item = req.body;
+    // create a db object
+    var itemForMongo = itemDB(req.body);
 
-    // asign a unique ID
-    item.id = lastId;
-    lastId += 1;
+
+    // validate the user is logged in
+    // the user has permission to create new items
+    // the item is not duplicated, not free
 
     // save the object
-    db.push(item);
+    itemForMongo.save(function (error, savedItem) {
+        if (error) {
+            console.log("Error saving item " + error);
+            res.status(500); // 500 Internal Server Error
+            res.send(error);
+        }
 
-    res.status(201); // Created!
-    res.json(item);
+        // no error
+        res.status(201); // 201 Created
+        res.json(savedItem);
+    });
 });
 
 app.get('/api/items', (req, res) => {
-    console.log("Client wants the DB");
-    res.json(db);
+    itemDB.find({}, function (error, data) {
+        if (error) {
+            res.status(500);
+            res.send(error);
+        }
+
+        // no error
+        res.json(data);
+    });
+});
+
+app.get('/api/items/:user', (req, res) => {
+    let name = req.params.user;
+
+    itemDB.find({ user: name }, function (error, data) {
+        if (error) {
+            res.status(500);
+            res.send(error);
+        }
+
+        // no error
+        res.json(data);
+    });
+
+});
+
+app.get('/api/items/search/:text', (req, res) => {
+    var text = req.params.text;
+    itemDB.find(
+        {
+            $or: [
+                { title: { "$regex": text, "$options": "i" } },
+                { description: { "$regex": text, "$options": "i" } }
+            ]
+        }
+        , function (error, data) {
+            if (error) {
+                res.status(500);
+                res.send(error);
+            }
+
+            // no error
+            res.json(data);
+        });
 });
 
 
-// Ctrl + p = Search
-// ctrl + b = hide/show files
-// run the server 3000
-// Localhost -> myself (computer)
-// 127.0.0.1 [home] -> myself
-// CORS => Cross Origin Resource Sharing
-app.listen(8080, function(){
+mongoDB.on('error', function (error) {
+    console.log("Db connection error: " + error);
+});
+
+mongoDB.on('open', function () {
+    console.log('Yeah, db connection opened');
+
+    /* The allowed SchemaTypes are:
+      String, Number, Date, Buffer, Boolean, Mixed, ObjectId, Array
+    */
+
+    // define schema for Db collection
+    var itemSchema = mongoose.Schema({
+        code: String,
+        title: String,
+        price: Number,
+        description: String,
+        category: String,
+        image: String,
+        user: String
+    });
+
+    // define order schema
+    var orderSchema = mongoose.Schema({
+        user: String,
+        total: Number,
+        status: Number,
+        items: Array
+    });
+
+    itemDB = mongoose.model("catCohort8", itemSchema);
+    orderDB = mongoose.model('ordersCohort8', orderSchema);
+
+});
+
+
+
+app.listen(8080, function () {
     console.log("Server running!! at http://localhost:8080");
 });
 
